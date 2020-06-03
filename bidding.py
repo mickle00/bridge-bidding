a641dcea6cc70ed8a2497f1f2f750613b306d09a
Michael Stewart <mikey@MikeyPro.local>
2020-06-02 21:05:08 -0700

    adding TODOs

--- bidding.py
+++ bidding.py
@@ -12,7 +12,7 @@ sum = clubs + diamonds + hearts + spades
 if (sum is not 13):
 	print ('Wrong number of cards, asshole.')
 
-print ('\n\nGathering High Card Info')
+print ('\nGathering High Card Info')
 print ('============================')
 
 aces = int(input ('Aces: '))
@@ -45,8 +45,7 @@ def get_balance():
 	if suits in balanced_hands:
 		return (balanced_hands[suits])
 	else:
-		return ('Unbalanced. Tipsy as a drunk elf')
-
+		return ('Drunk')
 
 def get_high_card_points():
 	return (
@@ -64,8 +63,56 @@ def get_total_points():
 
 	return get_high_card_points() + extra_points
 
-print('\n\nCrunching Numbers')
+## https://en.wikipedia.org/wiki/Bridge_Base_Basic#Opener_approximate_hand_strengths
+def get_opening_bid():
+	hcp = get_high_card_points()
+	if get_balance() is 'Drunk':
+		get_unbalanced_opening_bid(hcp)
+	else:
+		get_balanced_opening_bid(hcp)
+
+## For unbalanced hands:
+## 
+## 0 - 12 points: Pass unless the hand is suitable for a preemptive opening bid.
+## 13 - 21 points: Hands of 13 points or more are strong enough to open with natural bidding, including:
+## minimum opening hands with 13-15 points
+## medium opening hands with 16-18 points
+## maximum opening hands with 19-21 points
+## For unbalanced hands with 22+ points: show a very strong opening hand by using the strong 2♣ convention.
+
+def get_unbalanced_opening_bid(hcp):
+	print('Drunk Strategy')
+	if hcp <= 12:
+		print ('Pass')
+	elif hcp <= 21:
+		print ('wtf is natual bidding')
+	elif hcp > 21:
+		print ('Strong 2 Club Convention')
+
+
+## For balanced hands, open with a no-trump bid when you can limit your hand to the following point ranges:
+## 
+## 1NT = 15-17 HCP
+## 2NT = 20-21 HCP
+## 3NT = 25-27 HCP
+## For other balanced hands, you can still limit your points by opening in your longest suit and then using the no-trump bid on your second bid:
+## 
+## 13 - 14 HCP: bid 1 of a suit and then make a non-jump rebid in no trump (1NT)
+## 18 - 19 HCP: bid 1 of a suit and then make a jump rebid in no trump (2NT)
+## 22 - 24 HCP: bid 2♣ and then make a non-jump rebid in no trump (2NT)
+## 28 - 30 HCP: bid 2♣ and then make a jump rebid in no trump (3NT)
+## 31 - 32 HCP: bid 2♣ and then make a double-jump rebid in no trump (4NT)
+
+def get_balanced_opening_bid(hcp):
+	print ('TODO')
+
+print('\nCrunching Numbers')
 print ('============================')
 print ('Balance: ', get_balance())
 print ('HCP: ', get_high_card_points())
 print ('TP: ', get_total_points())
+
+print ('vhat to do, vhat to do....')
+print ('============================')
+print (get_opening_bid())
+
